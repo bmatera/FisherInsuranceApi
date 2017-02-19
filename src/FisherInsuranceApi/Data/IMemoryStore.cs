e806e1ca881acfa54bd8b54595d64cdb37b23006
Bill Matera <matera@optonline.net>
2017-02-19 16:40:53 -0500

    Lab 4, Exercise 0, Item 8 complete

---
+++ src/FisherInsuranceApi/Data/IMemoryStore.cs
@@ -0,0 +1,18 @@
+﻿using FisherInsuranceApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FisherInsuranceApi.Data
+{
+    public interface IMemoryStore
+    {
+        Quote CreateQuote(Quote quote);
+        IEnumerable<Quote> RetrieveAllQuotes { get; }
+        Quote RetrieveQuote(int id);
+        Quote UpdateQuote(Quote quote);
+        void DeleteQuote(int id);
+
+    }
+}
---
+++ src/FisherInsuranceApi/Models/Quote.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FisherInsuranceApi.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Product { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
